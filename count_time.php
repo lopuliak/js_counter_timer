373e49d1c3fffc5634e1be345c186bdfbb0c22e7
Oleh Lopuliak <oleh.lopuliak@gmail.com>
2017-07-05 18:47:50 +0300

    ready

--- count_time.php
+++ count_time.php
@@ -1,10 +1,15 @@
 <?php
 
+// file to save target date
 $saved_date = file_get_contents('time.txt');
+
+
+// $interval - interval in days
+$interval = 4;
 
 if (  strtotime("now") >= strtotime($saved_date) )
 {
-	$str_newDate = strtotime("+4 days");
+	$str_newDate = strtotime("+".$interval." days");
 	$newDate = date("Y/m/d" , $str_newDate);
 	file_put_contents('time.txt', $newDate);
 	echo $newDate;
